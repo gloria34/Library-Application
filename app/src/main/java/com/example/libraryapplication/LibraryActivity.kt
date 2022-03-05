45f141fe6a92e87aef3193f7d2e069fadde1d793
sofia <galkina@dlit.dp.ua>
2022-03-05 19:30:40 +0200

    test

--- app/src/main/java/com/example/libraryapplication/LibraryActivity.kt
+++ app/src/main/java/com/example/libraryapplication/LibraryActivity.kt
@@ -3,9 +3,9 @@ package com.example.libraryapplication
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class LibraryActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_library)
     }
 }
